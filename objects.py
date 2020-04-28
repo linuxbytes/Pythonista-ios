2c3b198a73f2bece96907bad160c46c01b300277
nixbytes <linuxbytes@users.noreply.github.com>
2020-04-27 21:57:21 -0400

    doc string

--- objects.py
+++ objects.py
@@ -1,5 +1,11 @@
 class Point:
+	"Represents a pointin two-dimensional geometirc coordinates"
 	def __init__(self, x, y):
+		"""
+		In intialize the postion of a new point.
+		The x and y coordinates can be specified. if they
+		are not, point defaults to the origins.
+		"""
 		self.move(x, y)
 
 	def move(self, x, y):
@@ -8,8 +14,11 @@ class Point:
 
 	def reset(self):
 		self.move(0, 0)
-
-
+	
+	def calculate_distance(self, other_point):	
+		"""
+		Calculate thr distance
+		"""	
 #	Contructing a Point
 
 point = Point(3, 5)
